6688e16a902d3cc3984f1ca14e8994137bd01c3c
BJonathan03 <bustinjonathan03@hotmail.com>
2019-01-05 12:44:27 +0100

    affichage d'un article

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use App\Entity\Ad;
 use App\Repository\AdRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdController extends AbstractController
@@ -13,14 +13,31 @@ class AdController extends AbstractController
     /**
      * @Route("/ads", name="ads_index")
      */
-    public function index(AdRepository $repo, SessionInterface $session)
+    public function index(AdRepository $repo)
     {
-        dump($session);
         //$repo = $this->getDoctrine()->getRepository(Ad::class);
+
         $ads = $repo->findAll();
 
         return $this->render('ad/index.html.twig', [
             'ads' => $ads,
         ]);
+    }
+
+    /**
+     * Permet d'afficher une seule annonce
+     *
+     * @Route("/ads/{slug}", name="ads_show")
+     *
+     * @return Response
+     */
+    public function show($slug, AdRepository $repo){
+        //findBy renvoie un tableau, findOneBy renvoie une seule réponse
+        $ad = $repo->findOneBySlug($slug);
+
+        return $this->render('ad/show.html.twig', [
+           'ad' => $ad
+        ]);
+
     }
 }
